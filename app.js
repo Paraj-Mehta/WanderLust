0f0681e5f3af7e7fce5a1f7544d413dd552ff1e4
Paraj Mehta <mehta.paraj007@gmail.com>
2025-07-26 18:58:25 +0530

    error handling 1

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const Listing = require("./models/listing.js");
+const express = require("express")
+const mongoose = require("mongoose")
+const Listing = require("./models/listing.js")
 const path = require("path");
-const methodOverride = require("method-override");
+const methodOverride = require("method-override")
 const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js")
+const ExpressError = require("./utils/ExpressError.js")
 const app = express();
 
 async function main(){
@@ -16,12 +18,13 @@ main().then(()=>{
     console.log(err);
 });
 
-app.set("view-engine","ejs");
+app.set("view engine","ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname,"/public")));
 app.engine("ejs", ejsMate);
+
 
 // Landing Page 
 app.get("/",(req,res)=>{
@@ -29,57 +32,78 @@ app.get("/",(req,res)=>{
 })
 
 
-// Home Page or All listing page
-app.get("/listings", async(req,res)=>{
+// Home Page for All listings
+app.get("/listings", wrapAsync(async(req,res)=>{
     const allListing = await Listing.find({});
     res.render("listings/index.ejs" , {allListing});
 })
+)
 
 
-// New form
+// To add a new Listing form
 app.get("/listings/add",(req,res)=>{
     res.render("listings/new.ejs");
 })
 
 
 // Show item page
-app.get("/listings/:id", async (req,res)=>{
+app.get("/listings/:id", wrapAsync(async (req,res)=>{
     let {id} = req.params;
     const item = await Listing.findById(id);
     res.render("listings/show.ejs", {item});
 })
+)
 
 
-// add new page
-app.post("/listings",async (req,res)=>{
+// All Listings page after adding a new Listing
+app.post("/listings", wrapAsync(async (req,res)=>{
+    if(!req.body.listing){
+        throw new ExpressError(400, "Send Valid data");
+    }
     let newListing = new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings")
 })
+)
 
 
-// edit route
-app.get("/listings/:id/edit", async (req,res)=>{
+// To edit Listing form
+app.get("/listings/:id/edit", wrapAsync(async (req,res)=>{
     let {id} = req.params;
     const item = await Listing.findById(id);
     res.render("listings/edit.ejs", {item});
 })
+)
 
 
-// edited routed
-app.put("/listings/:id", async (req,res)=>{
-    let {id} = req.params;
-    await Listing.findByIdAndUpdate(id ,{...req.body.listing}); 
-    res.redirect(`/listings/${id}`);
-})
+// Listing after it is edited 
+app.put("/listings/:id", wrapAsync(async (req, res, next)=>{
+        let {id} = req.params;
+        await Listing.findByIdAndUpdate(id ,{...req.body.listing}); 
+        res.redirect(`/listings/${id}`);
+    })
+)
 
 
 // delete route 
-app.delete("/listings/:id", async (req,res)=>{
+app.delete("/listings/:id", wrapAsync(async (req,res)=>{
     let {id} = req.params;
     await Listing.findByIdAndDelete(id);
     res.redirect("/listings");
 })
+)
+
+
+app.use((req, res, next)=>{
+    next(new ExpressError(404, "PAGE NOT FOUND"));
+});
+
+// Error middelware
+app.use((err, req, res, next)=>{
+    let {statusCode=500, message="Something went wrong"} = err
+    res.status(statusCode).send(message);
+})
+
 
 app.listen(8080, ()=>{
     console.log("app is listening on: localhost:8080/")
