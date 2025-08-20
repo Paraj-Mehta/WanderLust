c833912b8e23af5013e08daf17761bae36656660
Paraj Mehta <mehta.paraj007@gmail.com>
2025-08-20 21:15:46 +0530

    final push for deployment

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ const userRouter = require("./routes/user.js");
 
 // Session and cookie with flash
 const session = require("express-session");
-const sessionOptions = require("./init/secret.js");
+const MongoStore = require('connect-mongo');
+// const sessionOptions = require("./init/secret.js");
 const flash = require("connect-flash");
 
 // User authentication and authorization with passport 
@@ -25,15 +26,6 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
 
-async function main(){
-    await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
-}
-
-main().then(()=>{
-    console.log("Connected to DB")
-}).catch((err)=>{
-    console.log(err);
-});
 
 app.set("view engine","ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -42,11 +34,44 @@ app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname,"/public")));
 app.engine("ejs", ejsMate);
 
-// Landing Page 
-// app.get("/",(req,res)=>{
-//     res.send("Server is working");
-// })
+const dbUrl = process.env.ATLASDB_URL;
+console.log(dbUrl)
 
+async function main(){
+    await mongoose.connect(dbUrl);
+}
+
+main().then(()=>{
+    console.log("Connected to DB")
+}).catch((err)=>{
+    console.log(err);
+});
+
+
+// Mongo Store
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    crypto:{
+        secret: process.env.SECRET
+    },
+    touchAfter: 24 * 60 * 60
+})
+
+store.on("error" , ()=>{
+    console.log("ERROR in mongo store", err);
+})
+
+const sessionOptions = {
+    store: store,
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie:{
+        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpOnly : true
+    }
+};
 
 // Session and flash middlewares
 app.use(session(sessionOptions));
